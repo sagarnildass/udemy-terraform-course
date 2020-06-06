977fead81b0113de177a5a98e9c908da72b65e0f
sagarnildass@gmail.com <sagarnildass@gmail.com>
2020-06-07 01:17:34 +0530

    changed packer dir

--- 4_packer/3_terraform_with_packer_and_jenkins/scripts/jenkins-run-terraform.sh
+++ 4_packer/3_terraform_with_packer_and_jenkins/scripts/jenkins-run-terraform.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -ex
 AWS_REGION="ap-south-1"
-cd 4_packer
+cd 4_packer/3_terraform_with_packer_and_jenkins/
 S3_BUCKET=`aws s3 ls --region $AWS_REGION |grep terraform-state |tail -n1 |cut -d ' ' -f3`
-sed -i 's/terraform-state-1xytrh21/'${S3_BUCKET}'/'backend.tf
+sed -i 's/terraform-state-1xytrh21/'${S3_BUCKET}'/' backend.tf
 sed -i 's/#//g' backend.tf
 aws s3 cp s3://${S3_BUCKET}/amivar.tf amivar.tf --region $AWS_REGION
 terraform init
